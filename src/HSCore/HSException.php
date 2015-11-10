fb8344e9e065f1f61510a195bba8b42942521fc6
dw <husccexo@gmail.com>
2015-11-10 08:57:40 +0300

    Add user-friendly name for HSException

--- src/HSCore/HSException.php
+++ src/HSCore/HSException.php
@@ -4,4 +4,11 @@ namespace HSCore;
 
 class HSException extends \ErrorException {
 
+    /**
+     * @return string the user-friendly name of this exception
+     */
+    public function getName()
+    {
+        return 'PHP HandlerSocket Exception';
+    }
 }
